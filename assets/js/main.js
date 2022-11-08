f1544b53544a34f296272b29a832609344a638df
Matteogatto94 <cosenza94@live.it>
2022-11-08 16:08:55 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,17 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            emailUser: null,
+            emailUsers: [],
         }
     },
     mounted(){
         for (let index = 0; index < 10; index++) {
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-        console.log(response.data.response);
-    })
-            
-    }
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.emailUsers.push(response.data.response)
+            })
+        }
     }
 }).mount('#app')
