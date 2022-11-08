1dfdb00cec00755835bf58ee9b0d404bdfbf7cc6
Matteogatto94 <cosenza94@live.it>
2022-11-08 16:39:48 +0100

    add v-if condition for BONUS

---
+++ BONUS/assets/js/main.js
@@ -0,0 +1,24 @@
+/*Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.*/
+
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return{
+            emailUsers: [],
+        }
+    },
+    mounted(){
+        for (let index = 0; index < 10; index++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.emailUsers.push(response.data.response)
+            })
+        }
+
+        
+    }
+}).mount('#app')
